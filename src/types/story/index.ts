3a7333226ba7c2132080943fa5944fc26549ccd1
Serhii <dmtrnk.srg@gmail.com>
2025-06-23 21:55:47 +0300

    feat(DEV-8): add interfaces

--- src/types/story/index.ts
+++ src/types/story/index.ts
@@ -1,3 +1,5 @@
 export type { Story } from './story.interface';
 export type { StorySegment } from './story-segment.interface';
 export type { StoryTopic } from './story-topic.interface';
+export type { FindAllPaginatedOptions } from './find-all-paginated-options.interface';
+export type { CreateStoryPayload } from './create-story-payload.interface';
