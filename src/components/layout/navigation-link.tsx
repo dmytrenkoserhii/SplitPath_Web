842a6279e9175b3c09d5d1a6f754dfe08746205d
Serhii <dmtrnk.srg@gmail.com>
2025-06-23 21:56:33 +0300

    feat(DEV-8): add nav links and fix icon view

--- src/components/layout/navigation-link.tsx
+++ src/components/layout/navigation-link.tsx
@@ -1,11 +1,13 @@
-import { NavigationLink as NavigationLinkInterface } from '@/types/shared';
-import { NavLink } from '@mantine/core';
 import Link from 'next/link';
+
+import { NavLink } from '@mantine/core';
+
+import { NavigationLink as NavigationLinkInterface } from '@/types/shared';
 
 export const NavigationLink = ({ link }: { link: NavigationLinkInterface }) => {
   return (
     <span>
-      <NavLink component={Link} label={link.label} href={link.href} />
+      <NavLink component={Link} label={link.label} href={link.href} leftSection={link.icon} />
     </span>
   );
 };
