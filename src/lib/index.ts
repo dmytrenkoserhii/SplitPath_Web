6e17bf60b8f8ff7b2362aca027bb044e3b4ea936
Serhii <dmtrnk.srg@gmail.com>
2025-07-14 17:23:14 +0300

    feat(DEV-9): add socket for global chat

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 export { xiorClient } from './xior';
 export { queryClient } from './react-query';
-export { getFriendsSocket, getPrivateChatsSocket } from './sockets';
+export { getFriendsSocket, getPrivateChatsSocket, getGlobalChatSocket } from './sockets';
 export { formatDate } from './utils';
