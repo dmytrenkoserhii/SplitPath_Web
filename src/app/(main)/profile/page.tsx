3d866287b961dff94c131b6d593f435cd49f5a27
Serhii <dmtrnk.srg@gmail.com>
2025-07-18 15:59:53 +0300

    feat(DEV-10): add page and fix to layout

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,3 +1,46 @@
-export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+import { cookies } from 'next/headers';
+
+import { Stack, Title } from '@mantine/core';
+
+import { ServerError } from '@/components/auth';
+import { SettingsForm } from '@/components/profile';
+import { Account } from '@/types/user';
+
+export default async function ProfilePage() {
+  try {
+    let account: Account | null = null;
+    let error: string | null = null;
+
+    try {
+      const cookieStore = cookies();
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/account`, {
+        headers: {
+          Cookie: cookieStore.toString(),
+          'Content-Type': 'application/json',
+        },
+        next: { tags: ['account'] },
+      });
+
+      if (response.ok) {
+        account = await response.json();
+      } else {
+        error = 'Failed to load account data';
+      }
+    } catch (err) {
+      console.error('Server-side account fetch error:', err);
+      error = 'Failed to load account data';
+    }
+
+    return (
+      <Stack gap="xl">
+        <Title order={1} ta="center">
+          Profile Settings
+        </Title>
+
+        <SettingsForm account={account} error={error} />
+      </Stack>
+    );
+  } catch (error) {
+    return <ServerError error={error} />;
+  }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
-import '@mantine/core/styles.css';
-import './globals.css';
-
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
-import {
-  ColorSchemeScript,
-  MantineProvider,
-  mantineHtmlProps,
-} from '@mantine/core';
-import { theme } from '@/theme';
+
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 import { Notifications } from '@mantine/notifications';
+
 import { ReactQueryClientProvider } from '@/providers';
+import { theme } from '@/theme';
+
+import './globals.css';
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
@@ -28,13 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' {...mantineHtmlProps}>
+    <html lang="en" {...mantineHtmlProps}>
       <head>
-        <ColorSchemeScript defaultColorScheme='dark' />
+        <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body className={roboto.className}>
         <ReactQueryClientProvider>
-          <MantineProvider theme={theme} defaultColorScheme='dark'>
+          <MantineProvider theme={theme} defaultColorScheme="dark">
             <Notifications />
             {children}
           </MantineProvider>
